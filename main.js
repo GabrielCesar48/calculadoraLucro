d711dc387c0696f64676154ad0bc0fbad821bc8d
Gabriel Cesar <gabrielcesar48@gmail.comm>
2023-03-17 16:38:53 -0300

    adicionando validação de valores

--- main.js
+++ main.js
@@ -8,17 +8,45 @@ const percentualVendaRef = document.querySelector('#percentualVenda');
 
 const btnCalcularRef = document.querySelector('#btnCalcular');
 
+var validValorCompraRef = document.querySelector('#validValorCompra');
+
+var validValorVendaRef = document.querySelector('#validValorVenda')
+
+
+
+inputBuyRef.addEventListener('keyup', () => {
+    if (isNaN(inputBuyRef.value) || inputBuyRef.value == 0) {
+        validValorCompraRef.style.display = 'block'
+    } else {
+        validValorCompraRef.style.display = 'none'
+    }
+})
+
+inputSellRef.addEventListener('keyup', () => {
+    if (isNaN(inputSellRef.value) || inputSellRef.value == 0) {
+        validValorVendaRef.style.display = 'block'
+    } else {
+        validValorVendaRef.style.display = 'none'
+    }
+})
+
+
 
 
 
 btnCalcularRef.addEventListener('click',  () => {
 
-    var buyValor = parseFloat(inputBuyRef.value.replace(",","."))
-    var sellValor = parseFloat(inputSellRef.value.replace(",","."))
+    if (isNaN(inputBuyRef.value) || inputBuyRef.value == 0 || isNaN(inputSellRef.value) || inputSellRef.value == 0) {
+        alert('Você deve digitar um valor válido.')
+        let erroValor = `<p>Valor inválido</p>`
+        lucroVendaRef.innerHTML = erroValor
+        percentualVendaRef.innerHTML = erroValor        
 
-    // var buyValorFormated =parseFloat(buyValor.toFixed(2).replace(".",","));
-    // var sellValorFormated =parseFloat(sellValor.toFixed(2).replace(".",","));
-    
+    }
+
+
+    var buyValor = parseFloat(inputBuyRef.value.replace(",","."))
+    var sellValor = parseFloat(inputSellRef.value.replace(",","."))    
 
     var lucro = (sellValor - buyValor)
     var porcentagem = ((sellValor / buyValor)-1) *100
